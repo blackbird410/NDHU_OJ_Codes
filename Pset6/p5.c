78491a6ec4a9283d15572ae0ec18348c58059cf2
Neil T. Rigaud <ntsrigaud14@gmail.com>
2023-12-08 11:10:49 +0800

    Cleaned up the solution

--- Pset6/p5.c
+++ Pset6/p5.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+// Converts Arabic numeral to Roman Numeral
+
 #define SIZE 10000
 #define ROMAN_SIZE 13
 
-// Write a program to convert Arabic numbers to Roman numerals
 typedef struct Roman 
 {
 	char symbol;
 	int value;
 } Roman;
 
-// void init(Roman *r);
 void convert(int n, char *r);
 
 int main()
@@ -19,30 +19,12 @@ int main()
 	int n = 0;
 	char result[SIZE];
 
-	// Take input
 	scanf("%d", &n);
-
-	// Convert the number
 	convert(n, result);
-
-	// Display the result
 	printf("%s\n", result);
 	
 	return 0;	
 }
-
-// void init(Roman *r)
-// {
-// 	int i = 0;
-// 	const char ROMAN_LETTERS[][3] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-// 	const int ROMAN_VALUES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-// 	for (i = 0; i < ROMAN_SIZE; i++)
-// 	{
-// 		r[i].symbol = ROMAN_LETTERS[i];
-// 		r[i].symbol = ROMAN_VALUES[i];
-// 	}
-// }
 
 void convert(int n, char *result)
 {
@@ -52,20 +34,11 @@ void convert(int n, char *result)
 	const char ROMAN_LETTERS[][3] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 	const int ROMAN_VALUES[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-	// Roman r_nums[ROMAN_SIZE];
-
-	// Initialize the roman symbol structure
-	// init(r_nums);
-
 	while(n)
 	{
-		// d = r_nums[r_index].value;
-		// strcpy(l, &r_nums[r_index]->symbol);
-
 		d = ROMAN_VALUES[r_index];
 		strcpy(l, ROMAN_LETTERS[r_index]);
 
-		// Find the quotient
 		q = n / d;
 
 		// Is it > 0?
