748763eeb9102b5608e159cacf5c53db4a95c89e
Neil T. Rigaud <ntsrigaud14@gmail.com>
2023-12-18 15:22:14 +0800

    Solved week 10 problem 5

--- Pset10/p5.c
+++ Pset10/p5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SIZE 9
 #define BLACK 'X'
@@ -11,6 +12,9 @@ typedef struct Game
 	char board[SIZE][SIZE];
 	int black;
 	int white;
+	int count;
+	int isBlack;
+	int isWhite;
 } Game;
 
 void takeInput(Game *game);
@@ -18,121 +22,88 @@ void countPoints(Game *game);
 
 int main()
 {
-	Game *game = malloc(sizeof(Game));
-	if (game == NULL)
-		return 1;
+	Game game;
 
-	int n = 0;
-	scanf("%d\n", &n);
-	while(n--)
+	int cases;
+	scanf("%d\n", &cases);
+	while(cases--)
 	{
-		takeInput(game);
-		countPoints(game);
-		printf("Black %d White %d\n", game->black, game->white);
+		takeInput(&game);
+		countPoints(&game);
+		printf("Black %d White %d\n", game.black, game.white);
 	}
 
-	free(game);
-
 	return 0;
-}
-
-void emptyBuffer()
-{
-	while (getchar() != '\n');
 }
 
 void takeInput(Game *game)
 {
 	int i, j;
 
-	for (i = 0; i < SIZE; i++)
-	{
-		for (j = 0; j < SIZE; j++)
-			scanf("%c", &game->board[i][j]);
-		emptyBuffer();		
-	}
 	game->black = 0;
 	game->white = 0;
-}
 
-int isBound(int row, int col)
-{
-	return (row >= 0 && row < SIZE && col >= 0 && col < SIZE);
-}
-
-void updateCounter(Game *game, char player, int count)
-{
-	if (player == BLACK)
+	for (i = 0; i < SIZE; i++)
 	{
-		game->black += count;
+		gets(game->board[i]);
+		for (j = 0; j < SIZE; j++)
+		{
+			game->black = (game->board[i][j] == 'X') ? game->black + 1: game->black;
+			game->white = (game->board[i][j] == 'O') ? game->white + 1: game->white;
+		}	
+	}
+}
+
+int DFS(Game *game, int row, int col)
+{
+	if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) 
+		return 1;
+
+	if (game->board[row][col] == EMPTY)
+	{
+		game->board[row][col] = '*';
+		game->count += 1;
 	}
 	else
 	{
-		game->white += count;
+		if (game->board[row][col] == BLACK)
+			game->isBlack = 1;
+		else if (game->board[row][col] == WHITE) 
+			game->isWhite = 1;
+		return 0;
 	}
+
+	// Find the closest pieces
+	DFS(game, row - 1, col);
+	DFS(game, row + 1, col);
+	DFS(game, row, col - 1);
+	DFS(game, row, col + 1);
 }
-
-int isFree(Game *game, int row, int col)
-{
-	int w = 0, b = 0;
-
-	if (isBound(row, col - 1) && game->board[row][col - 1] != EMPTY)
-	{
-		w = (game->board[row][col - 1] == WHITE) ? w + 1 : w;
-		b = (game->board[row][col - 1] == BLACK) ? b + 1 : b;
-	}
-
-	if (isBound(row - 1, col) && game->board[row - 1][col] != EMPTY)
-	{
-		w = (game->board[row - 1][col] == WHITE) ? w + 1 : w;
-		b = (game->board[row - 1][col] == BLACK) ? b + 1 : b;
-	}
-	
-	if (isBound(row, col + 1) && game->board[row][col + 1] != EMPTY)
-	{
-		w = (game->board[row][col + 1] == WHITE) ? w + 1 : w;
-		b = (game->board[row][col + 1] == BLACK) ? b + 1 : b;
-	}
-
-	if (isBound(row + 1, col) && game->board[row + 1][col] != EMPTY)
-	{
-		w = (game->board[row + 1][col] == WHITE) ? w + 1 : w;
-		b = (game->board[row + 1][col] == BLACK) ? b + 1 : b;
-	}
-
-	if(w && b && w + b == 4)
-		return 1;
-	return 0;
-}
-
 
 void countPoints(Game *game)
 {
-	int count = 0, i, j;
+	int i, j;
 	char piece;
 
 	for (i = 0; i < SIZE; i++)
 	{
-		piece = game->board[i][j];
 		for (j = 0; j < SIZE; j++)
 		{
-			if (game->board[i][j] != piece)
+			if (game->board[i][j] == EMPTY)
 			{
-				if (piece == EMPTY)
-				{
-					piece = game->board[i][j];
-				}
-				else if (game->board[i][j] != EMPTY)
-				{
-					updateCounter(game, piece, count);
-					piece = game->board[i][j];
-					count = 0;
-				}
+				game->isBlack = 0;
+				game->isWhite = 0;
+				game->count = 0;
+				DFS(game, i, j);
+
+				if (game->isBlack && game->isWhite) 
+					continue;
+				if (game->isBlack) 
+					game->black += game->count;
+				else if (game->isWhite) 
+					game->white += game->count;
 			}
-			count = (game->board[i][j] != EMPTY || game->board[i][j] == EMPTY && !isFree(game, i, j)) ? count + 1: count;
 		}
-		updateCounter(game, piece, count);
-		count = 0;
 	}
 }
 
