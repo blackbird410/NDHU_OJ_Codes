516f1c330ed3ce20c3eade93b88104fa0a848df7
Neil Taïson Rigaud <112826785+blackbird410@users.noreply.github.com>
2023-12-21 22:19:08 +0000

    Solved week 11 problem 4

--- Pset11/p4.c
+++ Pset11/p4.c
@@ -8,16 +8,43 @@ int g(int x, int y);
 int eval(char *s);
 
 // F(G(F(2), 1))
-// F(G(7, 1))
+// F(G(7, F(1)))
 
 int main()
 {
     char s[SIZE];
-    fgets(s, SIZE, stdin);
+    gets(s);
     
     eval(s);
     printf("%s\n", s);
     return 0;
+}
+
+char *getNum(int j, char *n)
+{
+    int l = 0, i = 0;
+
+    while(j)
+    {
+        n[l] = j % 10 + '0';
+        j /= 10;
+        l++;
+    }
+    n[l] = '\0';
+
+    if (l == 1)
+        return n;
+
+    while(i < l / 2)
+    {
+        n[i] += n[l - i - 1];
+        n[l - i - 1] = n[i] - n[l - i - 1];
+        n[i] -= n[l - i - 1]; 
+
+        i++;
+    }
+
+    return n;
 }
 
 int eval(char *s)
@@ -25,12 +52,12 @@ int eval(char *s)
     if (s[0] >= '0' && s[0] <= '9')
         return 0;
     
-    printf("%s\n", s);
+    // printf("%s\n", s);
 
-    int i = 0, j = 0, pos = 0, x = 0, y = 0, k = 0;
-    char result[SIZE];
+    int i = 0, j = 0, pos = 0, x = 0, y = 0, k = 0, l = 0;
+    char result[SIZE], n[SIZE];
 
-    while(s[i] != '\n' && s[i] != '\0')
+    while(s[i] != '\0')
     {
         pos = (s[i] == 'F' || s[i] == 'G') ? i : pos;
         i++;
@@ -42,30 +69,34 @@ int eval(char *s)
     // Evaluate inner expression and replace in string
     if (s[pos] == 'F')
     {
+        // F(G(F(2), 1))
         // Get the x value
-        while (s[pos + k + 2] != ')')
+        k = 2;
+        while (s[pos + k] != ')')
         {
-            x = (x * 10) + (s[pos + k + 2] - '0');
+            x = (x * 10) + (s[pos + k] - '0');
             k++;
         }
         
         j = f(x);
         // Writing the result
-        while(j)
+        getNum(j, n);
+        while(n[l] != '\0')
         {
-            result[i] = j % 10 + '0';
-            j /= 10;
-            i++;
+            result[i + l] = n[l];
+            l++;
         }
+        i += l;
 
         // Writing rest of operation if there is any
-        pos += k + 3;
-        while (s[i] != '\n' && s[i] != '\0')
+        pos += k + 1;
+        while (s[pos] != '\0')
         {
             result[i] = s[pos];
             i++;
             pos++;
         }
+        result[i] = '\0';
 
         strcpy(s, result);
         eval(s);
@@ -74,7 +105,7 @@ int eval(char *s)
     {
         // F(G(7, 1))
         // Get the x value
-        k += 2;
+        k = 2;
         while (s[pos + k] != ',')
         {
             x = (x * 10) + (s[pos + k] - '0');
@@ -82,6 +113,7 @@ int eval(char *s)
         }
 
         // Get the y value
+        k += 2;
         while (s[pos + k] != ')')
         {
             y = (y * 10) + (s[pos + k] - '0');
@@ -90,29 +122,29 @@ int eval(char *s)
 
         j = g(x, y);
         // Writing the result
-        while(j)
+        getNum(j, n);
+        while(n[l] != '\0')
         {
-            result[i] = j % 10 + '0';
-            j /= 10;
-            i++;
+            result[i + l] = n[l];
+            l++;
         }
+        i += l;
 
         // Writing rest of operation if there is any
-        pos += k + 3;
-        while (s[i] != '\n' && s[i] != '\0')
+        pos += k + 1;
+        while (s[pos] != '\n' && s[pos] != '\0')
         {
             result[i] = s[pos];
             i++;
             pos++;
         }
+        result[i] = '\0';
 
         strcpy(s, result);
         eval(s);
     }
-    else
-    {
-        printf("Error\n");
-    }
+
+    return 1;
 }
 
 int f(int x)
