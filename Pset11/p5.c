060827c34f8d62caaa2f21aaf8985cb7bea8779a
Neil Taïson Rigaud <112826785+blackbird410@users.noreply.github.com>
2023-12-23 13:41:51 +0000

    Solved week 11 problem 5

--- Pset11/p5.c
+++ Pset11/p5.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #define SIZE 1000
 
@@ -10,68 +8,33 @@ description: Convert a decimal number @dec to the number with different @base an
 return vaule: Result after convert.
 notice: The letter you see in the number should be upper case.
 *************************************************************/
-char *shiftAdd(char *result, char r)
-{
-    int i, l = strlen(result);
-    if (!l)
-    {
-        result[0] = r;
-        result[1] = '\0';
-        return result;
-    }
-
-    // Shift the result and add the new digit
-    result[l + 1] = '\0';
-    for (i = l; i > 0; i--)
-        result[i] = result[i - 1];
-    result[0] = r;
-
-    return result;
-}
-
-char *reverse(char *result)
-{
-    int l = strlen(result), i = 0;
-
-    while(i < l/2)
-    {
-        result[i] += result[l - i - 1];
-        result[l - i - 1] = result[i] - result[l - i - 1];
-        result[i] -= result[l - i - 1];
-
-        i++;  
-    }
-
-    return result;
-}
-
-int isNum(char c)
-{
-    return (c >= '0' && c <= '9');
-}
 
 char *convertToDifferentBase(int dec, int base, char *result) 
 {
-    if (!dec)
+    if (!(dec / base))
     {
-        reverse(result);
-        return result;
-    }
+        if (dec < 0)
+        {
+            result[0] = '-';
+            result[1] = (abs(dec % base) > 9) ? abs(dec % base) + '7': abs(dec % base) + '0';
+            result[2] = '\0';
+        }
+        else
+        {
+            result[0] = (abs(dec % base) > 9) ? abs(dec % base) + '7': abs(dec % base) + '0';
+            result[1] = '\0';
+        }
+        
+        return result; 
+    }    
     
-    int r = abs(dec % base);
-    int l = (isNum(result[0])) ? strlen(result) : 0;
-    r = (r > 9) ? r + '7' : r + '0';
-    result[l] = r;
-    result[l + 1] = '\0';
-
-    if (dec < 0 && !(dec / base))
-    {
-        reverse(result);
-        shiftAdd(result, '-');
-        return result;
-    }
-
-    return convertToDifferentBase(dec / base, base, result);
+    convertToDifferentBase(dec / base, base, result);
+    int digit = (abs(dec % base) > 9) ? abs(dec % base) + '7': abs(dec % base) + '0';
+    int i = 0;
+    for (i = 0; result[i] != '\0'; i++);
+    result[i] = digit;
+    result[i+1] = '\0';
+    return result;
 }
 
 int main()
@@ -87,12 +50,3 @@ int main()
 
     return 0;
 }
-
-// 10     2     1010
-// 25     3     2201
-// 123     4     3003
-// 100     5     2020
-// 1024     8 10000000
-//     0    10        0
-// -10     2    -1010
-// -25     3    -2201
