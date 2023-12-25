7d1f303cf0f8f6bc8afef23dd0e35fcaf7e73182
Neil Taïson Rigaud <112826785+blackbird410@users.noreply.github.com>
2023-12-25 22:54:03 +0000

    Replace only new left-most targets

--- Pset11/p1.c
+++ Pset11/p1.c
@@ -14,6 +14,55 @@ int replaceTarget(char *source, char *target, char *rep, char *result)
 {
     // Replaces the first target found and return whether there was a new target created to the left
     
+    // Algorithm
+    // ----------
+    // Parse the source string while looking for the first character of the target
+    // If the first character is found, start checking the rest of the characters
+    // If all ther characters in target is found, then you can start replacing the target
+    // After replacing the target, add the rest of characters of the source string after the target replaced in the result string
+    char *p_source = source, *p_target, *p_rep = rep, *p_result = result;
+    int target_replaced = 0, pos = 0, i;
+
+    while(*p_source != '\0')
+    {
+        if (*p_source == *target && !target_replaced)
+        {
+            p_target = target;
+            i = 0;
+            // Start checking  for the rest of the characters
+            while(*p_target == *(p_source + i))
+                p_target++, i++;
+
+            if (*p_target == '\0')
+            {
+                pos = strlen(result);
+
+                // Replace the target
+                while(*p_rep != '\0')
+                {
+                    *p_result = *p_rep;
+                    p_result++, p_rep++;
+                } 
+                target_replaced = 1;
+                p_source += strlen(target);
+            }
+        }
+
+        *p_result = *p_source;
+        p_result++, p_source++;
+    }
+
+    // Check for new left-most target
+    if (target_replaced && pos)
+    {
+        i = 0;
+        pos--;
+        while(result[pos + i] == target[i])
+            i++;
+        return (target[i] == '\0');
+    }
+
+    return 0;
 }
 
 char *replace(char *source, char *target, char *replacement, int mode) 
@@ -46,12 +95,25 @@ char *replace(char *source, char *target, char *replacement, int mode)
     else
     {
         // Replace the first target found and replaces all new left target created in the result string
-        int pos = replaceTarget(source, target, replacement, result);
-        while(pos)
+        int isThereTarget = replaceTarget(source, target, replacement, result);
+        while(isThereTarget)
         {
-            replaceTarget(source, target, replacement, result);
+            strcpy(source, result);
+            isThereTarget = replaceTarget(source, target, replacement, result);
         }
     }
 
     return result;
+}
+
+int main()
+{
+    char source[] = "bbccbck", target[] = "bc", rep[] = "c", *result;
+    int mode = 1;
+
+    scanf("%d", &mode);
+    result = replace(source, target, rep, mode);
+    printf("%s\n", result);
+
+    return 0;  
 }
