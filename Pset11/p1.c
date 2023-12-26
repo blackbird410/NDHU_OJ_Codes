22efeb538830994b6fa4dd5315d0b65d73c4cdf2
Neil Taïson Rigaud <112826785+blackbird410@users.noreply.github.com>
2023-12-26 06:06:58 +0000

    Solved week 11 problem 1

--- Pset11/p1.c
+++ Pset11/p1.c
@@ -10,110 +10,50 @@ return value: Return the result after replace.
 *******************************************/
 #define SIZE 1000
 
-int replaceTarget(char *source, char *target, char *rep, char *result)
+char *replaceTarget(char *source, char *target, char *rep)
 {
-    // Replaces the first target found and return whether there was a new target created to the left
+    // Replaces the first target found and return the postion of the next target to replace
+    char *target_pos = strstr(source, target);
+    int pos = 0, i = 0;
+    char result[SIZE] = "";
+
+    if (!target_pos) return NULL;
     
-    // Algorithm
-    // ----------
-    // Parse the source string while looking for the first character of the target
-    // If the first character is found, start checking the rest of the characters
-    // If all ther characters in target is found, then you can start replacing the target
-    // After replacing the target, add the rest of characters of the source string after the target replaced in the result string
-    char *p_source = source, *p_target, *p_rep = rep, *p_result = result;
-    int target_replaced = 0, pos = 0, i;
+    strncat(result, source, target_pos - source);
+    strcat(result, rep);
+    strcat(result, target_pos + strlen(target));
+    strcpy(source, result);
 
-    while(*p_source != '\0')
-    {
-        if (*p_source == *target && !target_replaced)
-        {
-            p_target = target;
-            i = 0;
-            // Start checking  for the rest of the characters
-            while(*p_target == *(p_source + i))
-                p_target++, i++;
-
-            if (*p_target == '\0')
-            {
-                pos = strlen(result);
-
-                // Replace the target
-                while(*p_rep != '\0')
-                {
-                    *p_result = *p_rep;
-                    p_result++, p_rep++;
-                } 
-                target_replaced = 1;
-                p_source += strlen(target);
-            }
-        }
-
-        *p_result = *p_source;
-        p_result++, p_source++;
-    }
-
-    // Check for new left-most target
-    if (target_replaced && pos)
-    {
-        i = 0;
-        pos--;
-        while(result[pos + i] == target[i])
-            i++;
-        return (target[i] == '\0');
-    }
-
-    return 0;
+    return source;
 }
 
 char *replace(char *source, char *target, char *replacement, int mode) 
 {
-    // Decide of the mode 
-    // For mode 1:
-    // Parse the source string while checking for the target and assigning the characters in the result string
-    // When found replace it in the result string by the replacement string
-    // Input the rest of the characters after the first target into the result string 
-
-    // For mode 2:
-    // Starting from the left, replace the first target in the result string by the replacement 
-    // Check after replacing the target if there were not new target created
-    // If yes replace the created one 
-    // Continue to replace the left most targets in the result string if more are created
-    // Input the rest of the characters in the result string if there is any after replacing all targets in the source string
-    
-    // Return the result string with target replaced
-    char *result = malloc((SIZE + strlen(source)) * sizeof(char));
-    if (result == NULL)
+    char *target_pos = strstr(source, target);
+    if (!target_pos)
     {
-        return NULL;
+        return source;
     }
 
-    if (mode == 1)
+    while(target_pos != NULL)
     {
-        // Replace the first target found
-        replaceTarget(source, target, replacement, result);
+        target_pos = replaceTarget(source, target, replacement);
+        if (mode == 1) break;
     }
-    else
-    {
-        // Replace the first target found and replaces all new left target created in the result string
-        int isThereTarget = replaceTarget(source, target, replacement, result);
-        while(isThereTarget)
-        {
-            strcpy(source, result);
-            isThereTarget = replaceTarget(source, target, replacement, result);
-        }
-    }
-
-    return result;
+    return source;
 }
 
 int main()
 {
-    char source[] = "bbccbck", target[] = "bc", rep[] = "c", *result;
-    int mode = 1;
+    char source[] = "bbccbhaf";
+    char target[] = "bc";
+    char rep[] = "cb", *result;
 
-    scanf("%d", &mode);
-    result = replace(source, target, rep, mode);
-    printf("%s\n", result);
+    printf("Source: %s\n", source);
+    result = replace(source, target, rep, 1);
+    printf("Mode 1: %s\n", result);
+    result = replace(source, target, rep, 2);
+    printf("Mode 2: %s\n", result);
 
     return 0;  
 }
