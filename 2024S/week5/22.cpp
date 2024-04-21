daea84752948d6524d1faa6cd0b926746314387d
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-04-21 19:42:29 +0800

    Attempting problem 22

---
+++ 2024S/week5/22.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+class Date 
+{
+private:
+  int day, month, year;
+public:
+  Date(int d = 1, int m = 1, int y = 1900)
+  {
+    setYear(y);
+    setMonth(m);
+    setDay(d);
+  };
+  Date(std::string s) { getParams(s); };
+  ~Date() {};
+  void setDay( int d ) 
+  {
+    int mDays = getMonthDays(month, year);
+    day = !( d > mDays || d <= 0 ) 
+      ? d : throw std::out_of_range("Day value out of range.");
+  };
+
+  void setMonth( int m ) 
+  {
+    month = ( m > 0 && m < 13 ) 
+      ? m : throw std::out_of_range("Month value out of range.");
+  };
+
+  void setYear( int y )
+  {
+    year = ( y >= 1900 ) 
+      ? y : throw std::out_of_range("Year value must be at least 1900");
+  };
+
+  int getDay() { return day; };
+  int getMonth() { return month; };
+  int getYear() { return year; };
+
+  std::string toString() const 
+  {
+    std::string dateStr = std::to_string(year);
+    dateStr += ( month < 10 ) ? "/0" + std::to_string(month) : "/" + std::to_string(month);
+    dateStr += ( day < 10 ) ? "/0" + std::to_string(day) : "/" + std::to_string(day);
+
+    return dateStr;
+  }
+
+  int daysLeft()
+  {
+    // Finds the number of days left in the year
+    int days = getMonthDays(month, year) - day;
+    int m = month;
+    while( ++m < 13 )
+      days += getMonthDays(m, year);
+
+    return days;
+  };
+
+  int daysBetweenYears(int start, int end)
+  {
+    int days = 0;
+    while( ++start < end )
+      days += 365 + isLeapYear(start);
+    return days;
+  };
+
+  int getMonthDays(int m, int year) {
+    if (m == 2)
+      return 28 + isLeapYear(year);
+    return 30 + ((m <= 7 && (m % 2)) || (m > 7  && m % 2 == 0));
+  };
+
+  void getParams(std::string d) 
+  {
+    int i;
+    char str[12];
+    for(i = 0; i < d.length() && i < 12; i++) str[i] = d[i];
+    str[i] = '\0';
+
+    setYear(std::stoi(strtok(str, "/")));
+    setMonth(std::stoi(strtok(NULL, "/")));
+    setDay(std::stoi(strtok(NULL, "/")));
+  };
+
+  bool isLeapYear(int n) {
+    return (n % 400 == 0 || n % 100 && n % 4 == 0);
+  }
+
+  int operator-( Date& other )
+  {
+    int days = daysLeft();
+    days += daysBetweenYears(other.year, year + 1);
+    days -= other.daysLeft();
+
+    return days;
+  }
+
+  friend std::istream &operator>>( std::istream &input, Date &d )
+  {
+    std::string s;
+    input >> s;
+    d = Date(s);
+
+    return input;
+  }
+};
+
+int main() 
+{
+  Date d1, d2;
+
+  std::cin >> d1 >> d2;
+
+  std::cout << (d2 - d1) << std::endl;
+
+  std::cout << d1.toString() << std::endl;
+  std::cout << d2.toString() << std::endl;
+
+  return 0;
+}
+
